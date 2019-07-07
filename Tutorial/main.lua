c701c522b0f10d4b32178d5534b268d0ea6d46af
thorion21 <blazegamingro@gmail.com>
2019-07-08 00:34:29 +0300

    Added Vector2 Module

--- Tutorial/main.lua
+++ Tutorial/main.lua
@@ -1,1 +1,6 @@
-print("Hello World!")
+local Vector2 = require "Vector2"
+
+v1 = Vector2:new(1.7, 2.3)
+v2 = Vector2:new(3, 4)
+
+print(v1)
---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.window.width = 1024
+    t.window.height = 768
+    t.window.title = "Platformer"
+
+    t.console = true
+    t.modules.joystick = false
+end
