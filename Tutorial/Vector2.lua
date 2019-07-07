99ce3c44ea0c2f05036b624ec617c03070de111b
thorion21 <blazegamingro@gmail.com>
2019-07-08 00:50:55 +0300

    Vector2 Refactor

--- Tutorial/Vector2.lua
+++ Tutorial/Vector2.lua
@@ -1,46 +1,28 @@
 local Vector2 = {}
 
+--[[
+    Vector2 Module Implementation
+]]
+
 function Vector2:new(x, y)
     local vec = {
         x = x or 0,
         y = y or 0
     }
-
     self.__index = self
     return setmetatable(vec, self)
 end
 
-function Vector2:__add(other)
-    return Vector2:new(self.x + other.x, self.y + other.y)
-end
-
-function Vector2:__sub(other)
-    return Vector2:new(self.x - other.x, self.y - other.y)
-end
-
-function Vector2:__mul(other)
-    return Vector2:new(self.x * other.x, self.y * other.y)
-end
-
-function Vector2:__div(other)
-    return Vector2:new(self.x / other.x, self.y / other.y)
-end
-
-function Vector2:__eq(other)
-    return self.x == other.x and self.y == other.y
-end
-
-function Vector2:__tostring()
-    return string.format("<Vector2 %.2f, %.2f>", self.x, self.y)
-end
-
-function Vector2:copy()
-    return Vector2:new(self.x, self.y)
-end
-
-function Vector2:magnitude()
-    return math.sqrt((self.x ^ 2) + (self.y ^ 2))
-end
+function Vector2:__add(o) return Vector2:new(self.x + o.x, self.y + o.y) end
+function Vector2:__sub(o) return Vector2:new(self.x - o.x, self.y - o.y) end
+function Vector2:__mul(o) return Vector2:new(self.x * o.x, self.y * o.y) end
+function Vector2:__div(o) return Vector2:new(self.x / o.x, self.y / o.y) end
+function Vector2:__eq(o) return self.x == o.x and self.y == o.y end
+function Vector2:dot(o) return (self.x * o.x) + (self.y * o.y) end
+function Vector2:magnitude() return math.sqrt((self.x ^ 2) + (self.y ^ 2)) end
+function Vector2:copy() return Vector2:new(self.x, self.y) end
+function Vector2:zero() return Vector2:new() end
+function Vector2:__tostring() return string.format("<Vector2 %.2f, %.2f>", self.x, self.y) end
 
 function Vector2:normalize()
     local mag = self:magnitude()
@@ -48,10 +30,6 @@ function Vector2:normalize()
         self.x = self.x / mag
         self.y = self.y / mag
     end
-end
-
-function Vector2:dot(other)
-    return (self.x * other.x) + (self.y * other.y)
 end
 
 return Vector2
--- Tutorial/main.lua
+++ Tutorial/main.lua
@@ -4,3 +4,6 @@ v1 = Vector2:new(1.7, 2.3)
 v2 = Vector2:new(3, 4)
 
 print(v1)
+
+print(v1:normalize())
+print(v1)
